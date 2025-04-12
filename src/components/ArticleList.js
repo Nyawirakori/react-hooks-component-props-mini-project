f0dad61c6cc491c081fc444c21e24bce261b1646
Joan Nyawira Kori <korijoan171@gmail.com>
2025-04-12 22:48:05 +0300

    Mini app

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./Article"
+function ArticleList({posts}){
+    return(
+        <main>
+            {posts.map((posts) => (
+        <Article key={posts.id} title={posts.title} date={posts.date} preview={posts.preview} />
+            ))}
+    </main>
+    )
+}
+export default ArticleList;
